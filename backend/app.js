593862937ffb6d5090235f5116cba482aa05c251
michaus-coder <c14190168@john.petra.ac.id>
2023-02-23 05:05:33 +0700

    Change local port to 5000

--- backend/app.js
+++ backend/app.js
@@ -46,8 +46,8 @@ app.use(function(err, req, res, next) {
   res.send('error');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
 });
 
 export default app;
