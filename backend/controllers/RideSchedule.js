fbb6ea716cad533dde39de94c774511cb052ff26
michaus-coder <c14190168@john.petra.ac.id>
2023-04-11 09:18:30 +0700

    Add filter

--- backend/controllers/RideSchedule.js
+++ backend/controllers/RideSchedule.js
@@ -507,7 +507,12 @@ export const getRideScheduleByList = async (req, res) => {
               return rideRequest.user_id.user_id == req.query.user
             }) == undefined
           })
-        }
+          const singleDriver = driverArray.find((driver) => {
+            return driver.user_id == req.query.user;
+          });
+          rideScheduleArray = rideScheduleArray.filter((rideSchedule) => {
+            return rideSchedule.driver_id.driver_id == singleDriver.driver_id;
+          }
       }
 
       if (req.query.time !== undefined && req.query.time != "") {
